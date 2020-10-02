7bfc3eaa31e5d5c6679c7fba28a902c6d399924f
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-10-03 02:54:23 +0530

    [Shubham]Welcome

---
+++ src/main/java/com/capgi/addressbookusinggridle/AddressBookException.java
@@ -0,0 +1,7 @@
+package com.capgi.addressbookusinggridle;
+
+public class AddressBookException extends Exception {
+	public AddressBookException(String message) {
+		super(message);
+	}
+}
---
+++ src/main/java/com/capgi/addressbookusinggridle/Executor.java
@@ -0,0 +1,9 @@
+package com.capgi.addressbookusinggridle;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Addressbook");
+
+	}
+}

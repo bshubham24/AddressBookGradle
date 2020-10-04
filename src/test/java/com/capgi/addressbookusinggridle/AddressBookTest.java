c0e2cb5c97ca7cf7c4438e72f466168fe3ac7a71
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-10-04 18:49:19 +0530

    [Shubham]Added: Ability to add new contact by taking input from user

--- src/test/java/com/capgi/addressbookusinggridle/AddressBookTest.java
+++ src/test/java/com/capgi/addressbookusinggridle/AddressBookTest.java
@@ -3,56 +3,54 @@ package com.capgi.addressbookusinggridle;
 import org.junit.Assert;
 import org.junit.Test;
 
-
-
 public class AddressBookTest {
 
 	@Test
 	public void firstNameTest() {
 		Validation contact = new Validation();
 		boolean result = contact.ValidateFirstName("Shubham");
-		Assert.assertTrue(true);
+		Assert.assertTrue(result);
 	}
 
 	@Test
 	public void lastNameTest() {
 		Validation contact = new Validation();
 		boolean result = contact.ValidateLastName("Bhawsar");
-		Assert.assertTrue(true);
+		Assert.assertTrue(result);
 	}
 
 	@Test
 	public void emailTest() {
 		Validation contact = new Validation();
 		boolean result = contact.ValidateEmail("shubh007bhawsar@gmail.com");
-		Assert.assertTrue(true);
+		Assert.assertTrue(result);
 	}
 
 	@Test
 	public void phoneNoTest() {
 		Validation contact = new Validation();
 		boolean result = contact.ValidatePhoneNo("91 9874563214");
-		Assert.assertTrue(true);
+		Assert.assertTrue(result);
 	}
 
 	@Test
 	public void addressTest() {
 		Validation contact = new Validation();
 		boolean result = contact.ValidateAddress("whitehouse");
-		Assert.assertTrue(true);
+		Assert.assertTrue(result);
 	}
 
 	@Test
 	public void cityTest() {
 		Validation contact = new Validation();
 		boolean result = contact.ValidateCity("bomabay");
-		Assert.assertTrue(true);
+		Assert.assertTrue(result);
 	}
 
 	@Test
 	public void stateTest() {
 		Validation contact = new Validation();
 		boolean result = contact.ValidateState("punjab");
-		Assert.assertTrue(true);
+		Assert.assertTrue(result);
 	}
 }

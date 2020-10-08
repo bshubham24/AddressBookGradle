2f10c9a83521b890167442e614900fc673953c2b
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-10-09 01:42:50 +0530

    [Shubham]Added: Ability to search person in a city or state

--- src/main/java/com/capgi/addressbookusinggridle/Executor.java
+++ src/main/java/com/capgi/addressbookusinggridle/Executor.java
@@ -9,16 +9,19 @@ public class Executor {
 		Scanner sc = new Scanner(System.in);
 		int choice = 0;
 		System.out.println("Welcome to the address book");
-		while (choice != 3) {
+		while (choice != 9) {
 			System.out.println("Choose an option");
 			System.out.println("1. Create new Address Book");
 			System.out.println("2. Open an existing Address Book");
-			System.out.println("3. Quit");
+			System.out.println("5. Search a person's city or state");
+			System.out.println("9. Quit");
 			choice = Integer.parseInt(sc.nextLine());
 			if (choice == 1) {
 				multipleAddressBookObj.CreateAddressBook();
 			} else if (choice == 2) {
 				multipleAddressBookObj.OpenAddressBook();
+			} else if (choice == 5) {
+				multipleAddressBookObj.SearchCityOrStateByName1();
 			} else {
 				System.out.println("Thank You!");
 				break;

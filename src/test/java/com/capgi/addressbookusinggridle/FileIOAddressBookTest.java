e426f46ebd85c8b1d9d844c62b991530fb3a37fb
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-10-19 18:44:00 +0530

    [Shubham]Added: Ability to read and write contact into a file

---
+++ src/test/java/com/capgi/addressbookusinggridle/FileIOAddressBookTest.java
@@ -0,0 +1,32 @@
+package com.capgi.addressbookusinggridle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FileIOAddressBookTest {
+	@Test
+	public void givenContactsInFileShouldRead() {
+		FileIOServiceAddressBook fileIOServiceAddressBook = new FileIOServiceAddressBook();
+		List<Contacts> contactList = new ArrayList<>();
+		contactList = fileIOServiceAddressBook.readData();
+		System.out.println(contactList);
+		Assert.assertEquals(2, contactList.size());
+	}
+
+	@Test
+	public void writeContactsToFile() {
+		FileIOServiceAddressBook fileIOServiceAddressBook = new FileIOServiceAddressBook();
+		List<Contacts> contactList = new ArrayList<>();
+		Contacts contact1 = new Contacts("Shubham", "Bhawsar", "mp", "bhopal", "mp", "123456", "14 9478523699",
+				"shubham@gami.com");
+		Contacts contact2 = new Contacts("Wolverine", "Logan", "pune", "pune", "mh", "452136", "91 4785236998",
+				"afsdv@gmail.com");
+		contactList.add(contact1);
+		contactList.add(contact2);
+		fileIOServiceAddressBook.writeData(contactList);
+		Assert.assertEquals(16, fileIOServiceAddressBook.countEntries());
+	}
+}
